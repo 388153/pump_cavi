50e0353e8bff26049eede28e099bc584fc004041
388153 <patiparn@eng.buu.ac.th>
2026-03-05 14:57:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.svm import SVC
 st.subheader(':blue[Prediction of Cavitation in Centrifugal Pump ⚙️]')
 #Data Set Input to train and to test 
 
-st.warning(":ตัวอย่าง ค่าการสั่นสะเทือนในแนวแกน x, y, z และค่าเป้าหมาย (Target) เพื่อใชัในการเรียนรู้และทดสอบของโมเดล ")
+st.warning("ตัวอย่าง ค่าการสั่นสะเทือนในแนวแกน x, y, z และค่าเป้าหมาย (Target) เพื่อใชัในการเรียนรู้และทดสอบของโมเดล ")
 vibra_input = pd.read_csv('Cavi.csv')
 st.write(vibra_input.sample(3))
 
